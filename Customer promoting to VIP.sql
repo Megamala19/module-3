a33d07caeea21232b283b8a070aa6dd7b07c48c9
Megamala19 <mightymegu@gmail.com>
2026-07-11 12:59:42 +0530

    Create Customer promoting to VIP.sql

---
+++ Customer promoting to VIP.sql
@@ -0,0 +1,19 @@
+DECLARE
+    CURSOR vip_cursor IS
+        SELECT CustomerID, Balance
+        FROM Customers;
+
+BEGIN
+    FOR rec IN vip_cursor LOOP
+        IF rec.Balance > 10000 THEN
+            UPDATE Customers
+            SET IsVIP = 'TRUE'
+            WHERE CustomerID = rec.CustomerID;
+
+            DBMS_OUTPUT.PUT_LINE('Customer ' || rec.CustomerID || ' promoted to VIP.');
+        END IF;
+    END LOOP;
+
+    COMMIT;
+END;
+/
